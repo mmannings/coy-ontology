b426c456788fe48104e1937a29ecf0208d9f154c
TBoonX <tboonx@googlemail.com>
2022-08-31 14:25:26 +0200

    use a better function to detect version differences

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-function version_compare () { # made by https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash?page=2&tab=scoredesc#tab-top
-  function sub_ver () {
-    local len=${#1}
-    temp=${1%%"."*} && indexOf=`echo ${1%%"."*} | echo ${#temp}`
-    echo -e "${1:0:indexOf}"
-  }
-  function cut_dot () {
-    local offset=${#1}
-    local length=${#2}
-    echo -e "${2:((++offset)):length}"
-  }
-  if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "=" && exit 0
-  fi
-  local v1=`echo -e "${1}" | tr -d '[[:space:]]'`
-  local v2=`echo -e "${2}" | tr -d '[[:space:]]'`
-  local v1_sub=`sub_ver $v1`
-  local v2_sub=`sub_ver $v2`
-  if (( v1_sub > v2_sub )); then
-    echo ">"
-  elif (( v1_sub < v2_sub )); then
-    echo "<"
-  else
-    version_compare `cut_dot $v1_sub $v1` `cut_dot $v2_sub $v2`
-  fi
-}
-
 function owl_version_compare () {
   local file=$1
   versionInfo=`cat $file | grep owl:versionInfo | head -n 1 | grep -oP '[\.\d{1,6}]*'`
@@ -35,9 +8,9 @@ function owl_version_compare () {
   echo $versionInfo
   echo $priorVersion
 
-  temp=$(version_compare $versionInfo $priorVersion)
+  temp=$(dpkg --compare-versions $versionInfo "gt" $priorVersion && echo "yes" || echo "no")
   echo $temp
-  if [[ $temp != ">" ]]
+  if [[ $temp != "yes" ]]
   then
     echo "file $file : owl:versionInfo is not higher than owl:priorVersion!"
     exit 1
@@ -50,12 +23,12 @@ function owl_version_increase_test () {
   versionInfo_file1=`cat $file1 | grep owl:versionInfo | head -n 1 | grep -oP '[\.\d{1,6}]*'`
   versionInfo_file2=`cat $file2 | grep owl:versionInfo | head -n 1 | grep -oP '[\.\d{1,6}]*'`
 
-  echo $versionInfo_file1
-  echo $versionInfo_file2
+  echo "version file 1: $versionInfo_file1"
+  echo "version file 2: $versionInfo_file2"
 
-  temp=$(version_compare $versionInfo_file1 $versionInfo_file2)
+  temp=$(dpkg --compare-versions $versionInfo_file1 "lt" $versionInfo_file2 && echo "yes" || echo "no")
   echo $temp
-  if [[ $temp != "<" ]]
+  if [[ $temp != "yes" ]]
   then
     echo "file $file : owl:versionInfo of $file1 is not lower than of $file2!"
     exit 1
